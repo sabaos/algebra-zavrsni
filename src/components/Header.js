b30eb50287eb914932c0ae43d81af438047e5658
Mario <sabinmejl@gmail.com>
2023-05-14 10:28:39 +0200

    lkj

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) =>{
         {/* Prazan react fragment, služi istu svrhu kao i div, ali manje opterećuje DOM */}
         {isLoggedIn && 
         <div className="App-header">
-        <button className='sidebar-btn' onClick={props.toggleSidebar}>Sidebar</button>
+        <button className='sidebar-btn' onClick={props.toggleSidebar}>Izbornik</button>
         <h1>{props.username}'s Chat Room</h1>
         <button onClick={props.handleLogout}>Logout</button>
       </div>}
