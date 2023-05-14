15e80f59198a2232560ab942654c1dfa1bb1710c
Mario <sabinmejl@gmail.com>
2023-05-14 10:19:14 +0200

    update

--- src/components/Input.js
+++ src/components/Input.js
@@ -29,7 +29,7 @@ class Input extends Component {
             // bilo autofocus="true"
             autoFocus={true}
           />
-          <button>Send</button>
+          <button className="btn-send">Send</button>
         </form>
       </div>
     );
