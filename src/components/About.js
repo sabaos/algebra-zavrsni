6615d46732c8eaef98605739bfae85ef5b46a875
Mario <sabinmejl@gmail.com>
2023-05-19 22:45:05 +0200

    final

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,25 @@
+import "../style.css";
+
 const About = () =>{
-    return <div><h1>About Page</h1>
+    return <div className="background">
+        <div className="wrapper">
+            <h1 className="title">"Chatty" - gdje razgovori postaju stvarnost!</h1>
+            
+            <div className="about1">Naša vizija je stvoriti siguran i poticajan digitalni prostor gdje razgovori postaju autentični odnosi. "Chatty" je mjesto gdje možete upoznati nove ljude, dijeliti ideje i otkriti inspiraciju iz različitih kultura i pozadina.
+</div>
+            <div className="about2">Vaša privatnost i sigurnost su naš prioritet. Uvijek se trudimo osigurati sigurno okruženje i čuvati vaše podatke tajno. Naši algoritmi također rade na tome da vam pomognu u pronalaženju "srodne duše" - ljudi s kojima dijelite iste vrijednosti i interese. Možda ćete pronaći prijatelja za cijeli život ili čak ljubav svog života putem "Chatty"-ja.</div>
+
+            <div className="about3">Postanite dio naše globalne zajednice i doživite svijet na potpuno nov način. Bez obzira želite li razgovarati o strastima, dobiti savjet ili proširiti svoje vidike, "Chatty" je tu da vas podrži i poveže s ljudima koji će vam pružiti jedinstveno iskustvo.
+</div>
+
+<img src={require('./img/chatty_icon_about.png')} className='img1'></img>
+<img src={require('./img/verified.png')} className='img2'></img>
+<img src={require('./img/global.png')} className='img3'></img>
+            </div>
+
+            
+        
+        
     </div>
 }
 export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Header = (props) =>{
     /* !! ispred props.username pretvara vrijednost u boolean vrijednost */
     const isLoggedIn = !!props.username;
@@ -8,14 +10,18 @@ const Header = (props) =>{
         <div className="App-header">
           <div className='left-section' >
           <div className='logo-section' >
-            <img src={require('./img/chatty_icon.png')} className='img-height'></img>
+            <img src={require('./img/chatty_icon.png') } alt='logo' className='img-height'></img>
             <h1>Chatty</h1>
             </div>
             <div className='user-section'><p>Korisnik:</p> <p className='username'>{props.username}</p></div>
         </div>
         <div className='right-section'>
-          <button  onClick={props.toggleSidebar}>Izbornik</button>
-          <button  onClick={props.handleLogout}>Logout</button>
+        <ul className="main-menu">
+                        <li><Link to="/algebra-zavrsni/" >Chat</Link></li>
+                        <li><Link to="/algebra-zavrsni/about" >About</Link></li>
+                        <li onClick={props.handleLogout} ><Link to="#">Logout</Link></li>
+                    </ul>
+
         </div>
       </div>}
 
